9c4c547be10f92ab4c5487dcefe2e4b6bd5012f2
JoshJ-OSU <johnsjo3@onid.oregonstate.edu>
2015-11-17 10:03:37 -0800

    Assignment 5 Initial

---
+++ projects/johnsjo3/dominion/BugsInTeammates.c
@@ -0,0 +1,21 @@
+Introduction
+============
+For the following tests, I used my unit tests and card tests developed over the past few weeks.
+The tests were largely developed as random tests with the exception being the test for the
+adventurer card.  This week I adapted the tests to the new dominion.c file and functions and
+ran the tests.
+
+Teammate 1
+==========
+Copied Project Folder from dmachowi
+-
+
+
+
+Teammate 2
+==========
+Copied Project Folder from henningb
+-
+
+Conclusions
+===========
