8c5bc9f4b26caf9c68982c4e12a58915cfa253e4
Harrings <sean.harrington2@gmail.com>
2015-11-19 19:12:37 -0800

    Ran tests on teammates code

---
+++ projects/harrings/dominion/bugsinteamate.c
@@ -0,0 +1,44 @@
+Bugs in Toko
+Bug 1 
+Adventurer Card does not increase handsize
+where
+executeAdventurerCard() function
+description
+The adventurer card does not cause hand size to increase by 1 as it should
+
+
+
+Bug 2 
+Smithy Card does not increase handsize by 2
+where
+executesmithyCard() function
+description
+The smithy card does not increase handsize by 2 as it should
+
+
+
+Bug 3 
+Village card does not increase actions by 2
+where
+executevillagecard() function
+description
+The village card does not increase actions by 2 as it should
+
+
+
+Bug 4 
+Council room card did not increase buys by 1
+where
+executecouncilcard() function
+description
+The council room card does not increase buys by 1 as it should
+
+
+
+Bugs in Toro
+Bug 1 
+Adventurer Card does not increase handsize
+where 
+adventurerCard() function
+description
+The adventurer card does not cause hand size to increase by 1 as it should
---
+++ projects/harrings/teammate2/dominion/bugsinteammate2.c
@@ -0,0 +1,10 @@
+Bugs in Toro
+Bug 1 
+Adventurer Card does not increase handsize
+where 
+adventurerCard() function
+description
+The adventurer card does not cause hand size to increase by 1 as it should
+
+This was the only Bug I found in this code
+
