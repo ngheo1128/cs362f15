79762bb12b97caa796122fd3a31ac9cb85907ff3
Scott Milton <miltons@oregonstate.edu>
2015-11-20 18:14:40 -0800

    Remove testDrawCard and badTestDrawCard from make all option in makefiles for teammates codebases. Modify randomtestcard.c and randomtestadventurer.c in teammate 2 folder to acommodate refactored code for player number.

---
+++ projects/miltons/teammate1Dominion/BugsInTeammate1.c
@@ -0,0 +1,29 @@
+/*
+
+Teammate 1 name: Ashok Nayar
+
+issues:
+
+UNIT TESTS:
+
+
+
+CARD TESTS:
+
+compiler warnings:
+
+cardtest1 and cardtest2 give compiler warnings for
+    unused variables in cardEffect function of dominion.c:
+    z, cardDrawn, drawntreasure, temphand, x
+
+cardtest2:
+    /bin/sh: line 1: 31639 Segmentation fault (core dumped)
+        cardtest2 >> unittestresults.out
+        make: *** [unittestresults.out] Error 139
+
+
+RANDOM TESTS:
+
+
+
+*/
