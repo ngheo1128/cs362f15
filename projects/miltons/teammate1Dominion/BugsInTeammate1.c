729a0d910075468181588da4c8d05d4f1ec36557
Scott Milton <miltons@oregonstate.edu>
2015-11-21 20:04:28 -0800

    Fix flaw in randomtestcard.c.

--- projects/miltons/teammate1Dominion/BugsInTeammate1.c
+++ projects/miltons/teammate1Dominion/BugsInTeammate1.c
@@ -8,23 +8,28 @@ UNIT TESTS:
 
 
 
+
 CARD TESTS:
 
-compiler warnings:
-
-cardtest1 and cardtest2 give compiler warnings for
+compiler warnings: cardtest1 and cardtest2 give compiler warnings for
     unused variables in cardEffect function of dominion.c:
     z, cardDrawn, drawntreasure, temphand, x
 
-cardtest2: (Adventurer)
+TEST NAME    CARD NAME     RESULT
+cardtest1    Smithy        PASS
+cardtest2:   Adventurer    FAIL
     /bin/sh: line 1: 31639 Segmentation fault (core dumped)
         cardtest2 >> unittestresults.out
         make: *** [unittestresults.out] Error 139
+cardtest3
 
 
 RANDOM TESTS:
 
-randomtestadventurer.c: (Adventurer)
+randomtestcard:  FAIL for all 2000 random tests
+                 (Smithy did not add the expected number of cards to hand)
+
+randomtestadventurer: FAIL
     /bin/sh: line 1: 11054 Segmentatoin fault (core dumped)
         randomtestadventurer >> randomtestadventurer.out
         make: *** [randomtestadventurer.out] Error 139
