fdbf0acf62e3989d2a09b90b5aea8a958fba531a
mcdokath <mcdokath@oregonstate.edu>
2015-11-22 02:01:47 -0800

    Finished BugFixes.c

--- projects/mcdokath/dominion/BugFixes.c
+++ projects/mcdokath/dominion/BugFixes.c
@@ -7,6 +7,11 @@ The bugs that both of my teammates found are the bugs that I introduced:
 Namely, that my smithy function adds an extra card to the player's hand,
 and that my adventurer function only allows the player to draw one treasure card.
 
-I fixed my buggy smithy function by changing the for loop condition to < instead of <=.
+Bugs I introduced that my teammates did not report are as follows:
+The great hall function does not add +1 action like it should.
+The embargo function discards the card rather than trashes it.
 
+I fixed my buggy smithy function by changing the for loop condition to < instead of <=.
 I fixed my buggy adventurer function by initializing drawnTreasure at 0 instead of 1.
+I fixed my buggy great hall function by adding code to add +1 action.
+I fixed my embargo function by changing discardCard to 1, which trashes the card.
