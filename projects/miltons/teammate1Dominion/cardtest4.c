d4d7aa659fb66f6ebb01e7e92afcb0c88cfaba2e
Scott Milton <miltons@oregonstate.edu>
2015-11-22 14:31:35 -0800

    Touch up to cardtest3 in teammate1 folder.

--- projects/miltons/teammate1Dominion/cardtest4.c
+++ projects/miltons/teammate1Dominion/cardtest4.c
@@ -1,25 +1,21 @@
-// tests Mine card
-// int mineEffect(int currentPlayer, struct gameState *state, int handPos, int choice1, int choice2)
-
+// tests Adventurer card
+// int adventurerEffect(int player, struct gameState *state)
 #include<stdio.h>
 #include "dominion.h"
 #include "dominion_helpers.h"
-//#include "rngs.h"
 
 int main(int argc, char *argv[])
 {
+    struct gameState testState;
 
-    // Ashok Nayar did not choose the mine card for his assignment.
-
-    // struct gameState testState;
-    // int retVal = mineEffect(0, &testState, 0, 0, 0);
-    // if (retVal == 0)
-    // {
-    //     printf("Mine card: PASS\n");
-    // }
-    // else
-    // {
-    //     printf("Mine card: FAIL\n");
-    // }
+    int retVal = adventurerEffect(0, &testState);
+    if (retVal == 0)
+    {
+        printf("Adventurer card: PASS\n");
+    }
+    else
+    {
+        printf("Adventurer card: FAIL\n");
+    }
     return 0;
 }
