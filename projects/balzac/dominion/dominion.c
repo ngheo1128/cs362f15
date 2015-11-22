709539308225ec350946e0a268b708c67ddf7a29
Casey Balza <caseybalza@gmail.com>
2015-11-22 12:34:25 -0800

    Fixed bugs teammates found

--- projects/balzac/dominion/dominion.c
+++ projects/balzac/dominion/dominion.c
@@ -211,7 +211,7 @@ int shuffle(int player, struct gameState *state) {
   qsort ((void*)(state->deck[player]), state->deckCount[player], sizeof(int), compare); 
   /* SORT CARDS IN DECK TO ENSURE DETERMINISM! */
 
-  while (state->deckCount[player] <= 0) {
+  while (state->deckCount[player] > 0) {
     card = floor(Random() * state->deckCount[player]);
     newDeck[newDeckPos] = state->deck[player][card];
     newDeckPos++;
@@ -426,7 +426,7 @@ int scoreFor (int player, struct gameState *state) {
       if (state->hand[player][i] == duchy) { score = score + 3; };
       if (state->hand[player][i] == province) { score = score + 6; };
       if (state->hand[player][i] == great_hall) { score = score + 1; };
-      if (state->hand[player][i] == gardens) { score = score + ( fullDeckCount(player, 0, state) / 10 ); };
+      if (state->hand[player][i] == gardens) { score = score + ( fullDeckCount(player, i, state) / 10 ); };
     }
 
   //score from discard
@@ -437,7 +437,7 @@ int scoreFor (int player, struct gameState *state) {
       if (state->discard[player][i] == duchy) { score = score + 3; };
       if (state->discard[player][i] == province) { score = score + 6; };
       if (state->discard[player][i] == great_hall) { score = score + 1; };
-      if (state->discard[player][i] == gardens) { score = score + ( fullDeckCount(player, 0, state) / 10 ); };
+      if (state->discard[player][i] == gardens) { score = score + ( fullDeckCount(player, i, state) / 10 ); };
     }
 
   //score from deck
@@ -448,7 +448,7 @@ int scoreFor (int player, struct gameState *state) {
       if (state->deck[player][i] == duchy) { score = score + 3; };
       if (state->deck[player][i] == province) { score = score + 6; };
       if (state->deck[player][i] == great_hall) { score = score + 1; };
-      if (state->deck[player][i] == gardens) { score = score + ( fullDeckCount(player, 0, state) / 10 ); };
+      if (state->deck[player][i] == gardens) { score = score + ( fullDeckCount(player, i, state) / 10 ); };
     }
 
   return score;
@@ -1159,7 +1159,7 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state, int tra
   else 	
     {
       //replace discarded card with last card in hand
-      state->hand[currentPlayer][handPos] = state->hand[currentPlayer][ (state->handCount[currentPlayer] - 1)];
+      state->hand[currentPlayer][handPos] = state->hand[currentPlayer][(state->handCount[currentPlayer] - 1)];
       //set last card to -1
       state->hand[currentPlayer][state->handCount[currentPlayer] - 1] = -1;
       //reduce number of cards in hand
@@ -1246,10 +1246,13 @@ int adventurer_card(int currentPlayer, int cardDrawn, int drawntreasure, int z, 
 		 state->deckCount[currentPlayer] = state->discardCount[currentPlayer];
 		 shuffle(currentPlayer, state);
 		}
+		
 		drawCard(currentPlayer, state);
 		cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
 		if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+		{
 		  drawntreasure++;
+		}
 		else{
 		  temphand[z]=cardDrawn;
 		  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
