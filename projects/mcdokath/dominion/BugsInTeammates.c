7135b9773ff962908693a75a70478f98a2aeeae6
mcdokath <mcdokath@users.noreply.github.com>
2015-11-22 01:34:51 -0800

    Update BugsInTeammates.c

--- projects/mcdokath/dominion/BugsInTeammates.c
+++ projects/mcdokath/dominion/BugsInTeammates.c
@@ -4,28 +4,51 @@
  */
 
 /* Weckwerth */
-cardtest1 results:
-Pass: Reworked smithy function
+cardtest1 results (smithy):
 Fail: Hand count (4)
 Pass: Number of actions
 Pass: Coins
 Pass: Number of buys
 
-cardtest2 results:
-Pass: Reworked adventurer function
+cardtest2 results (adventurer):
 Fail: Hand count (5)
 
-cardtest3 results:
+cardtest3 results (village):
+Pass: +1 card
+Fail: +2 actions
 
-cardtest4 results:
+cardtest4 results (remodel):
+Pass: Hand count
 
 unittest1 results:
+All tests passed!
+Testing players 0-1 with 1-5 cards and 0-10 bonus
 
 unittest2 results:
+All tests passed!
+Pass: Player 0 deckCount unchanged
+Pass: Player 1 deckCount unchanged
+Pass: whoseTurn unchanged
+Pass: Phase unchanged
+Pass: Coins unchanged
+Pass: numBuys unchanged
+Pass: Player 0 handCount unchanged
+Pass: Player 1 handCount unchanged
 
 unittest3 results:
+All tests passed!
+Pass: Testing regular outcome
+Pass: Not enough buys
+Pass: Not enough cards
+Pass: Not enough coins
 
 unittest4 results:
+All tests passed!
+Pass: Success test in discard pile
+Pass: Success test in deck
+Pass: Success test in hand
+Pass: Test not enough cards
+Pass: Test card not in game
 
 randomtestadventurer results:
 Fail: handCount (numPlayers: 3, tested player: 0)
