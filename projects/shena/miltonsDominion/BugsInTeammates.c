079e2dfd2dad873907f44bbb4c36099a6fd81508
Andrew Shen <andrewdshen@gmail.com>
2015-11-22 16:43:43 -0600

    completed tests for teammate. added BugsInTeammates.c

--- projects/shena/miltonsDominion/BugsInTeammates.c
+++ projects/shena/miltonsDominion/BugsInTeammates.c
@@ -1,31 +1,49 @@
 /*
+MiltonS Test Results
+====================
 
-Me
-smithyEffect
-adventurerEffect
-feastEffect
-councilRoomEffect
+cardtest1.c
+	This tested the Smithy Card. Test ran without a hitch.
+	Bugs found:
+		1. Card count not correctly increased
 
-mineEffect
+cardtest2.c
+	This tested the Adventurer Card. Test ran without a hitch.
+	Bugs found:
+		1. Deck not properly resized
+		2. Cards are not properly discarded
+		
+cardtest3.c
+	This tested the Village Card. Test ran without a hitch. No bugs were found.
+	
+cardtest4.c
+	This tested the Great Hall Card. Test ran without a hitch.
+	Bugs found:
+		1. Card count not correctly adjusted
+		2. numActions not correctly increased
+	
+unittest1.c
+		This tested the isGameOver() function. Test ran without a hitch. No bugs were found.
 
--------------
+unittest2.c
+	This tested the supplyCount() funciton. Test ran without a hitch. No bugs were found.
 
-Teammate1
-smithyEffect
-adventurerEffect
-feastEffect
-councilEffect
+unittest3.c
+	This tested the shuffle() function. Test ran without a hitch. No bugs were found.
+	
+unittest4.c
+	This tested the newGame() function. Test ran without a hitch. No bugs were found.
 
-villageEffect
+randomtestadventurer.c
+	This was a random test for the Adventurer Card. All 1000 tests were completed.
+	812/1000 tests failed.
+	Bugs found:
+		1. Failed to increase number of cards by 2.
 
--------------
-
-Teammate2
-smithyEffect
-adventurerEffect
-feastEffect
-
-villageEffect
-great_hallEffect
-
+randomtestcard.c
+	This was a random card test for the Steward Card.
+	1000/1000 tests failed.
+	Bugs found:
+		1. Failed to increaes number of cards by 2.
+		2. Failed to trash 2 cards.
 */
