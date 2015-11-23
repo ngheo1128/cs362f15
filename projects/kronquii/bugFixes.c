8f3e8d7bd23188ade12db6c73101295d8966fc75
Ian Kronquist <iankronquist@teleport.com>
2015-11-22 17:57:28 -0800

    Does anyone else look at the commit messages?

---
+++ projects/kronquii/bugFixes.c
@@ -0,0 +1,5 @@
+1. The function updateCoins is not declared in the dominin.h header. This causes a link-time warning. Fixed by addion updateCoins to the header file.
+2. The adventurer card is not discarded. Fixed by adding a call to 'discardCard'.
+4. The smithy card does not draw the right number of cards. Fixed by changing the loop conditions.
+5. The council room does not add another buy. Another simple fix.
+6. The arguments to the villagerEffect function are passed in the wrong order. Fixed by changing the order of the arguments.
