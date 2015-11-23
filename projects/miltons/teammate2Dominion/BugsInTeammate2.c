d3ecfd394cd5f4a26e97fa933dea7a1cc40da434
Scott Milton <miltons@oregonstate.edu>
2015-11-22 16:05:13 -0800

    Add information to BugsInTeammates.c.

--- projects/miltons/teammate2Dominion/BugsInTeammate2.c
+++ projects/miltons/teammate2Dominion/BugsInTeammate2.c
@@ -22,12 +22,15 @@ CARD TESTS:
 
 TEST NAME    CARD NAME     RESULT                         COVERAGE
 cardtest1    Smithy        PASS                            6.12 %
-cardtest2    Adventurer    PASS                           20.24 %
+cardtest2    Adventurer    PASS                           17.92 %
 cardtest3    Council Room  NOT REFACTORED / NOT TESTED     0.00 %
 cardtest4    Mine Card     NOT REFACTORED / NOT TESTED     0.00 %
 
 
 RANDOM TESTS:
+
+TEST NAME             RESULT                              COVERAGE
+randomtestcard:       FAIL for all 2000 random tests      20.75 %
 
 randomtestcard:  FAIL for all 2000 random tests
                  (Smithy did not add expected number of cards to player's hand)
@@ -40,20 +43,20 @@ randomtestcard:  FAIL for all 2000 random tests
     The loop should iterate only three times in order to create the desired
     game behavior by adding 3 cards to the player's hand (not 4).
 
+TEST NAME             RESULT                              COVERAGE
+randomtestadventurer: PASS for all 2000 random tests      21.84 %
 
-randomtestadventurer: FAIL
-/bin/sh: line 1: 8676 Segmentation fault (core dumped)
-    randomtestadventurer >> randomtestadventurer.out
-    make: *** [randomtestadventurer.out] Error 139
-
-
-    The fault is located 
-
-    while(drawntreasure<2){
+    Note: visual inspection found a logical error not detected by the tests:
+    The error is located in adventurerEffect on line 664:
 
       if (cardDrawn == copper || cardDrawn == silver)
-        drawntreasure++;
 
-Note: playdom won't run either from segfault error
+    The line should read:
+
+        if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+
+    This line is missing the gold condition (in other words, it only checks for copper or silver).
+    Although this error is not causing the game to crash, the function should check for all three
+    treasure cards to generate the desired behavior.
 
 */
