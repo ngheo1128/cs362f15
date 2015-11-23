85d5c526d938d85f62b90fe5132dc33e470b1125
unknown <maderb@onid.oregonstate.edu>
2015-11-22 21:38:19 -0800

    Assignment 5

---
+++ projects/maderb/dominion/BugsInTeammates.c
@@ -0,0 +1,29 @@
+/*
+smithyEffect():
+	Does not properly discard smithy card.
+		no cards in discard (discardCount==0, discard[0]=??)
+	Not enough cards with cards remaining in deck
+	Played: 96 Cards in Hand: -400 Final Deck Count: 200
+
+getWinners():
+	P1 does not win with higher score
+	Players do not tie with same score when Player 2's turn on completion.
+
+outpostEffect():
+	Card not added to discard pile correctly.
+
+adventurerEffect(): 
+	Unsuccesful stop at 2 treasure cards.
+	Card not added to discard pile correctly.
+
+salvagerEffect():
+	Error during discard of salvager or targeted card.
+
+smithyEffect():
+	Does not discard smithy properly.
+
+scoreFor():
+	Incorrect value from discard.
+	Incorrect value from deck.
+	Incorrect value from garden.
+*/	
