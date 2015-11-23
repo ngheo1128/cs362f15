bffcec5ab210629acce5528562f074ab51e28ac8
Ian Kronquist <iankronquist@teleport.com>
2015-11-22 17:59:52 -0800

    foo
    Man, this class...

--- projects/kronquii/bugFixes.c
+++ projects/kronquii/bugFixes.c
@@ -3,3 +3,5 @@
 4. The smithy card does not draw the right number of cards. Fixed by changing the loop conditions.
 5. The council room does not add another buy. Another simple fix.
 6. The arguments to the villagerEffect function are passed in the wrong order. Fixed by changing the order of the arguments.
+
+None of my teammates reported any bugs to me.
