a0ea833bb411b0cd219a832513ff2d83173725bc
jpgourley <jpgourley@gmail.com>
2015-12-04 14:22:16 -0800

    Updated Project Test Files

--- projects/gourleja/URLValidator/src/UrlValidatorTest.java
+++ projects/gourleja/URLValidator/src/UrlValidatorTest.java
@@ -19,13 +19,13 @@
 import junit.framework.TestCase;
 
 
-
-
-
 /**
- * Performs Validation Test for url validations.
+ * CS362-400 - Final Project Part B
+ * Jason Gourley
+ * James Riccelli
+ * Michael Walker
  *
- * @version $Revision: 1128446 $ $Date: 2011-05-27 13:29:27 -0700 (Fri, 27 May 2011) $
+ * Performs Validation Test for url validations.
  */
 public class UrlValidatorTest extends TestCase {
 
@@ -41,7 +41,42 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
+
+       ResultPair[] testAuthority = {
+    		   new ResultPair("www.oregonstate.edu", true),
+               new ResultPair("oregonstate.edu", true),
+               new ResultPair("-oregonstate.edu", false),
+               new ResultPair("oregonstate-.edu", false),
+               new ResultPair("oregon-state.edu", true),
+               new ResultPair("oregonstate", false),
+               new ResultPair("test.com", true),
+               new ResultPair("test.us", true),
+               new ResultPair("test.biz", true),
+               new ResultPair("test.tv", true),
+               new ResultPair("", false)};
+
+       
+       final String testScheme = "http://";
+       int errors = 0;
+
+       for (int i = 0; i < testAuthority.length; i++) 
+       {
+           String newUrl = testScheme + testAuthority[i].item;
+           boolean expected = testAuthority[i].valid;
+           boolean result = urlVal.isValid(newUrl);
+           
+           if(result == expected){
+        	   System.out.println(newUrl);  
+           } else { 
+        	   //assertEquals(newUrl, expected, result);
+        	   System.err.println(newUrl);  
+        	   errors++;
+           }
+       }
+       if (errors > 0) {
+    	   String message = "Errors = " + errors; 
+    	   assertEquals(message, 0, errors);
+       }		   
 	   
    }
    
@@ -58,27 +93,105 @@ public class UrlValidatorTest extends TestCase {
     		   new ResultPair("http://", true),
     		   new ResultPair("HTTP://", true),
                new ResultPair("https://", true),
-               new ResultPair("ftp://", true),
+               new ResultPair("HTTPS://", true),
                new ResultPair("aaa://", true),
                new ResultPair("aaas://", true),
+               new ResultPair("about://", true),
+               new ResultPair("acap://", true),
+               new ResultPair("acct://", true),
                new ResultPair("cap://", true),
-               new ResultPair("chrome-extension://", true),
                new ResultPair("cid://", true),
+               new ResultPair("coap://", true),
+               new ResultPair("coaps://", true),
+               new ResultPair("chrome-extension://", true),
+               new ResultPair("crid://", true),
                new ResultPair("data://", true),
+               new ResultPair("dav://", true),
                new ResultPair("dict://", true),
-    		   new ResultPair("imap://", true),
+               new ResultPair("dns://", true),
+               new ResultPair("example://", true),
+               new ResultPair("file://", true),
+               new ResultPair("ftp://", true),
+               new ResultPair("geo://", true),
+               new ResultPair("go://", true),
+               new ResultPair("gopher://", true),
+               new ResultPair("h323://", true),
+               new ResultPair("iax://", true),
+               new ResultPair("icap://", true),
+               new ResultPair("im://", true),
+               new ResultPair("imap://", true),
+               new ResultPair("info://", true),
+               new ResultPair("ipp://", true),
+               new ResultPair("ipps://", true),
+               new ResultPair("iris://", true),
+               new ResultPair("iris.beep://", true),
+               new ResultPair("iris.lwz://", true),
+               new ResultPair("iris.xpc://", true),
+               new ResultPair("iris.xpcs://", true),
+               new ResultPair("jabber://", true),
+               new ResultPair("ldap://", true),
+               new ResultPair("mailto://", true),
+               new ResultPair("mid://", true),
     		   new ResultPair("ms-secondary-screen-controller://", true),
+    		   new ResultPair("msrp://", true),
+    		   new ResultPair("mtqp://", true),
+    		   new ResultPair("mupdate://", true),
+    		   new ResultPair("news://", true),
+    		   new ResultPair("nfs://", true),
+    		   new ResultPair("ni://", true),
+    		   new ResultPair("nih://", true),
+    		   new ResultPair("nntp://", true),
+    		   new ResultPair("opaquelocktoken://", true),
+    		   new ResultPair("pkcs11://", true),
+    		   new ResultPair("pop://", true),
+    		   new ResultPair("pres://", true),
+    		   new ResultPair("reload://", true),
+    		   new ResultPair("rtsp://", true),
+    		   new ResultPair("rtsps://", true),
+    		   new ResultPair("rtspu://", true),
+    		   new ResultPair("service://", true),
+    		   new ResultPair("session://", true),
+    		   new ResultPair("shttp://", true),
+    		   new ResultPair("sieve://", true),
+    		   new ResultPair("sip://", true),
+    		   new ResultPair("sips://", true),
+    		   new ResultPair("sms://", true),
+    		   new ResultPair("snmp://", true),
     		   new ResultPair("soap.beep://", true),
+    		   new ResultPair("soap.beeps://", true),
+    		   new ResultPair("stun://", true),
+    		   new ResultPair("stuns://", true),
+    		   new ResultPair("tag://", true),
+    		   new ResultPair("tel://", true),
+    		   new ResultPair("telnet://", true),
+    		   new ResultPair("tftp://", true),
+    		   new ResultPair("thismessage://", true),
+    		   new ResultPair("tip://", true),
+    		   new ResultPair("tn3270://", true),
+    		   new ResultPair("turn://", true),
+    		   new ResultPair("turns://", true),
+    		   new ResultPair("tv://", true),
+    		   new ResultPair("urn://", true),
+    		   new ResultPair("vemmi://", true),
+    		   new ResultPair("ws://", true),
+    		   new ResultPair("wss://", true),
+    		   new ResultPair("xcon://", true),
+    		   new ResultPair("xcon-userid://", true),
+    		   new ResultPair("xmlrpc.beep://", true),
+    		   new ResultPair("xmlrpc.beeps://", true),
+    		   new ResultPair("xmpp://", true),
     		   new ResultPair("z39.50r://", true),
     		   new ResultPair("z39.50s://", true),
     		   new ResultPair("123://", false),
     		   new ResultPair("a://", true),
+    		   new ResultPair("http:///", false),
     		   new ResultPair("http:/", false),
                new ResultPair("http:", false),
                new ResultPair("http/", false),
+               new ResultPair("http:\\", false),
+               new ResultPair("http:\\\\", false),
                new ResultPair("://", false),
                new ResultPair("", true)};
-       
        
        final String testServer = "oregonstate";
        final String testTld = ".edu";
@@ -94,7 +207,7 @@ public class UrlValidatorTest extends TestCase {
         	   System.out.println(newUrl);  
            } else { 
         	   //assertEquals(newUrl, expected, result);
-        	   System.err.println(newUrl + "\n");  
+        	   System.err.println(newUrl);  
         	   errors++;
            }
        }
@@ -104,8 +217,89 @@ public class UrlValidatorTest extends TestCase {
        }
    }
    
+   /*
+    * Partition test focused on URL Domain Host, Label, and TLD combinations
+    * 
+    * */   
    public void testYourSecondPartition(){
-	   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       
+       ResultPair[] testAuthority = {
+    		   new ResultPair("", false),
+    		   new ResultPair("a", false),
+               new ResultPair("a.b", false),
+               new ResultPair("a.b.c", false),
+               new ResultPair("a.b.c.d", false),
+               new ResultPair("a.b.c.d.com", true),
+               new ResultPair("a.b.c.com", true),
+               new ResultPair("a.b.com", true),
+               new ResultPair("a.com", true),       
+               new ResultPair("-a.com", false),
+               new ResultPair("a-.com", false),
+               new ResultPair("a-b.com", true),
+               new ResultPair("a-b-.com", false),
+               new ResultPair("a-b-c.com", true),
+               new ResultPair("a-b-c-.com", false),
+               new ResultPair("a-b-c-d.com", true),
+               new ResultPair("a_b_c_d.com", false),
+               new ResultPair("a_b_c_.com", false),
+               new ResultPair("_a_b_c.com", false),
+               new ResultPair("192.168.1.1", true),
+               new ResultPair("192.168.1", false),
+               new ResultPair("192.168", false),
+               new ResultPair("192", false),
+               new ResultPair("0", false),
+               new ResultPair("0.", false),
+               new ResultPair("0.0", false),
+               new ResultPair("0.0.", false),
+               new ResultPair("0.0.0", false),
+               new ResultPair("0.0.0.", false),
+               new ResultPair("0.0.0.0", true),
+               new ResultPair("255.255.255.255", true),
+               new ResultPair("256.256.256.256", false),
+               new ResultPair("255.255.255.255.255", false),
+               new ResultPair("255.255.255.255.", false),
+               new ResultPair("255.com", true),               
+               new ResultPair(".com", false),
+               new ResultPair("test.aa", false),
+               new ResultPair("test.a", false),
+               new ResultPair("test.123", false),
+               new ResultPair("test.1", false),
+               new ResultPair("1.com", true),
+               new ResultPair("1.us", false),
+               new ResultPair("1abc.com", true),
+               new ResultPair("abc1.com", true),
+               new ResultPair("1abc.us", true),
+               new ResultPair("abc1.us", true),
+               new ResultPair("aaa.", false),
+               new ResultPair(".aaa", false),
+               new ResultPair("aaa", false),
+               new ResultPair("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com", true),
+               new ResultPair("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com", true),
+               new ResultPair("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl.com", false)};
+
+       final String testScheme = "http://";
+       
+       int errors = 0;
+
+       for (int i = 0; i < testAuthority.length; i++) 
+       {
+           String newUrl = testScheme + testAuthority[i].item;
+           boolean expected = testAuthority[i].valid;
+           boolean result = urlVal.isValid(newUrl);
+           
+           if(result == expected){
+        	   System.out.println(newUrl);  
+           } else { 
+        	   //assertEquals(newUrl, expected, result);
+        	   System.err.println(newUrl);  
+        	   errors++;
+           }
+       }
+       if (errors > 0) {
+    	   String message = "Errors = " + errors; 
+    	   assertEquals(message, 0, errors);
+       }	   
    }
    
    
